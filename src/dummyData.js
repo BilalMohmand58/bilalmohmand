a9ac2bf7923acda2b1aac461d160f84949c78d2c
Bilal Ahmad <bilalahmadkhan1729@gmail.com>
2022-08-23 21:28:41 +0500

    project added

--- src/dummyData.js
+++ src/dummyData.js
@@ -7,8 +7,9 @@ export const featuredPortfolio = [
   },
   {
     id: 2,
-    title: "Rampa UI Design",
-    img: "https://cdn.dribbble.com/users/702789/screenshots/15054318/media/4ea5d492b7b07eebc9528ff960794879.png?compress=1&resize=1200x900",
+    title: "Touristoo App",
+    img: "materials/touristoo.png",
+    link: "https://touristoo.herokuapp.com/",
   },
   {
     id: 3,
