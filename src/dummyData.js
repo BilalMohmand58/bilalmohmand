41aaf458b7637bc709d414b7f46829a7b63ec958
Bilal Ahmad <bilalahmadkhan1729@gmail.com>
2022-09-13 16:54:09 +0500

    final

--- src/dummyData.js
+++ src/dummyData.js
@@ -1,7 +1,7 @@
 export const featuredPortfolio = [
   {
     id: 1,
-    title: "RollaDice Game",
+    title: "Dice Game Web",
     img: "https://img.freepik.com/free-vector/casino-background-design_1314-202.jpg?w=740&t=st=1660746578~exp=1660747178~hmac=507723d9169459e5c4435cae1d5501f6beb5115fc9ae0c5bd25bed2f0ec73e63",
     link: "https://rolldice.up.railway.app/",
   },
