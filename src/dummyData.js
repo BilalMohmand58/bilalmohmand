5663cd0407123d7e7892f1f17898287e576ffbd3
Bilal Ahmad <bilalahmadkhan1729@gmail.com>
2022-09-18 00:10:05 +0500

    final tweaks

--- src/dummyData.js
+++ src/dummyData.js
@@ -1,15 +1,29 @@
 export const featuredPortfolio = [
   {
     id: 1,
+    title: "Touristoo App",
+    img: "materials/touristoo.png",
+    link: "https://touristoo.cyclic.app/",
+  },
+
+  {
+    id: 2,
+    title: "Blog App MERN",
+    img: "materials/blog.png",
+    link: "https://bprogrammer.herokuapp.com/",
+  },
+  {
+    id: 3,
     title: "Dice Game Web",
     img: "https://img.freepik.com/free-vector/casino-background-design_1314-202.jpg?w=740&t=st=1660746578~exp=1660747178~hmac=507723d9169459e5c4435cae1d5501f6beb5115fc9ae0c5bd25bed2f0ec73e63",
     link: "https://rolldice.up.railway.app/",
   },
+
   {
-    id: 2,
-    title: "Touristoo App",
-    img: "materials/touristoo.png",
-    link: "https://touristoo.cyclic.app/",
+    id: 4,
+    title: "Coffee Shop UI",
+    img: "materials/coffeeShopUI.png",
+    link: "https://bilalmohmand58.github.io/coffeeShopReact/",
   },
 ];
 
@@ -29,8 +43,9 @@ export const mobilePortfolio = [
 export const designPortfolio = [
   {
     id: 1,
-    title: "Project",
-    img: "materials/future1.png",
+    title: "Coffee Shop UI",
+    img: "materials/coffeeShopUI.png",
+    link: "https://bilalmohmand58.github.io/coffeeShopReact/",
   },
   {
     id: 2,
